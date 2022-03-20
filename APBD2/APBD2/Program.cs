b8d3730e133256195a3d6aeaa5a8b8cb6163d379
danjab-jd <jd@pja.edu.pl>
2022-03-20 12:55:20 +0100

    Reading file

--- APBD2/APBD2/Program.cs
+++ APBD2/APBD2/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace APBD2
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var path = args[0];
+
+            //var fi = new FileInfo(path);
+            FileInfo fi = new(path);
+
+            var fileContent = new List<string>();
+
+            using (StreamReader stream = new(fi.OpenRead()))
+            {
+                // Analogicznie co ""
+                //string line = string.Empty;
+
+                string line = null;
+
+                while ((line = await stream.ReadLineAsync()) != null)
+                {
+                    fileContent.Add(line);
+                }
+                //stream.Dispose();
+            }
+
+            foreach (var item in fileContent)
+            {
+                Console.WriteLine(item);
+            }
+
+            //foreach (var item in File.ReadLines(path)){}
+
+            // DateTime - typ dla daty 
+            // DateTime.Parse("2022-03-20")
+
+            // string.IsNullOrWhiteSpace(str)
+
+            // StreamWriter stream... stream.WriteLine(line)
+
+            //var json = JsonSerializer.Serialize(set)
+
+
         }
     }
 }
